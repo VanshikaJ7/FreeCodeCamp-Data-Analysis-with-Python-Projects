ac17e60f6344367f134799a816fe8d581a007490
VanshikaJ7 <64067645+VanshikaJ7@users.noreply.github.com>
2026-01-23 12:41:00 +0530

    update

---
+++ Medical Data Visualizer/main.py
@@ -0,0 +1,8 @@
+# This entrypoint file to be used in development. Start by reading README.md
+import medical_data_visualizer
+from unittest import main
+
+medical_data_visualizer.draw_cat_plot()
+medical_data_visualizer.draw_heat_map()
+
+main(module='test_module', exit=False)
